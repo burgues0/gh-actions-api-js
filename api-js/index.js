51850937494222e4d7223566e36db46edb8c1688
burgues0 <victordpp@outlook.com>
2026-04-11 23:20:43 -0300

    fix: zap scan 2

--- api-js/index.js
+++ api-js/index.js
@@ -6,7 +6,24 @@ import db from './database.js';
 const app = express();
 const port = 3000;
 
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+    },
+  },
+  hsts: {
+    maxAge: 31536000,
+    includeSubDomains: true,
+    preload: true,
+  },
+}));
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
 app.use(express.json());
 app.use('/', router);
 
